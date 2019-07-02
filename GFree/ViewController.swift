ec2ad3ab023145c4677f3cc172438a7ff24c3fe9
MJ <mym0404@gmail.com>
2019-07-03 02:09:29 +0900

    init commit

---
+++ GFree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GFree
+//
+//  Created by 문명주 on 03/07/2019.
+//  Copyright © 2019 MJ Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
